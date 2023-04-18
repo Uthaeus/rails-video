57629397d29f9408904ab72d691bbf929adc8b14
Uthaeus <romanlavery@gmail.com>
2023-04-18 14:01:56 -0600

    added admin verification for edit, destroy links for projects, added styling to project show

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 6.times do |i|
     Project.create!(
         title: "Project #{i}",
-        description: "This is project #{i}",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         thumbnail: "https://via.placeholder.com/400x250",
         video_url: "example.com"
     )
@@ -13,7 +13,8 @@ puts "6 projects created"
     User.create!(
         email: "test@test.com",
         password: "foobar",
-        password_confirmation: "foobar"
+        password_confirmation: "foobar",
+        role: "site_admin"
     )
 end
 
