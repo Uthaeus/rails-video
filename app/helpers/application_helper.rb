3a68b4a14dd8e8585b332b79176c68c53e201f64
Uthaeus <romanlavery@gmail.com>
2023-04-18 16:09:02 -0600

    created image helper function, added styling to project form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+    def image_generator(height:, width:)
+        "http://placehold.it/#{height}x#{width}"
+    end
+
+    def portfolio_img img
+        if img.length != 0
+            img 
+        else
+            image_generator(height: '400', width: '250')
+        end
+            
+    end
 end
